6618d326be1e419e35c24113dc2fae3670cd29e5
rieznikk <oleksandr.rieznikk@gmail.com>
2024-08-15 22:11:26 +0300

    fix(ngrok): removed

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
-  transpileDependencies: true,
-  devServer: {
-    allowedHosts: 'all', // Allow all hosts
-    host: '0.0.0.0', // Listen on all network interfaces
-    port: 8080, // Ensure this matches the port Ngrok is forwarding
-    client: {
-      webSocketURL: {
-        protocol: 'wss', // Use secure WebSocket protocol
-        hostname: 'a863-45-128-38-171.ngrok-free.app', // Your Ngrok hostname
-        port: 443, // Standard HTTPS port
-        pathname: '/ws', // Pathname for the WebSocket
-      },
-    },
-  },
+  transpileDependencies: true
 });
