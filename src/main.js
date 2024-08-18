2a51a14fbb3032441749934d2a5d9c787fe8e07e
rieznikk <oleksandr.rieznikk@gmail.com>
2024-08-18 17:06:50 +0300

    style(form): changed paymentForm & paymentFormEdit

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import modal from './plugins/modal-window';
 import contextMenu from "./plugins/context-menu";
 import PaymentFormEdit from "./plugins/payment-form-edit";
 import vuetify from './plugins/vuetify';
+import './assets/styles/index.scss';
 
 Vue.config.productionTip = false;
 Vue.use(modal);
